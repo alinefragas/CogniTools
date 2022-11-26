e7bf96711510f533789ad293fb2c912809a15791
António Ramiro <antonio.ramiro@tecnico.ulisboa.pt>
2022-11-26 19:33:44 +0000

    MVP games

---
+++ LetterSoup.py
@@ -0,0 +1,76 @@
+#Start a python GUI
+#Import the tkinter module
+from tkinter import *
+import random
+window = Tk()
+window.title("Letter Soup")
+window.geometry("900x700")
+#tkinter bigger font
+font1 = ("Times", 24, "bold")
+#Create a label
+
+target = 0
+window.configure(background='white')
+
+#Create a label
+palavra = "FAringe"
+#convert to upper case
+palavra = palavra.upper()
+
+#Place the word on top
+label = Label(window, text = palavra, font=font1)
+
+#break a string into a list of strings with each word as an item
+
+letters = list(palavra)
+print(letters)
+listButtons = []
+
+
+
+
+listX= random.sample(range(50,850), len(letters))
+for i,letter in enumerate(letters):
+    x = listX[i]
+    print(i)
+    y = random.randint(50, 650)
+
+   
+    listButtons.append( Button(window, text = str(letter), font=font1, command= lambda w= i :onClick(w)))
+    listButtons[i].place(x = x, y = y)
+    window.update()
+    
+
+print(listButtons)
+#delete button from screen
+
+
+
+def onClick(i):
+    global target
+    if i == target:
+       
+        print(letters[i])
+        listButtons[i].destroy()
+        target = target + 1
+    else:
+        #play sound in tkinter
+        print("wrong")
+        window.bell()
+
+
+    
+
+#Display the label
+label.pack()
+#Start the event loop
+
+#create a button that disapears if you click it
+button = Button(window, text = "Click Me", command = Button.destroy)
+button.pack()
+
+
+window.mainloop()
+
+
+
---
+++ LexicalRace.py
@@ -0,0 +1,83 @@
+import re
+import urllib.request
+from collections import Counter
+
+theme = 'Football'
+URL = 'https://en.wikipedia.org/wiki/' + theme
+
+
+counter = Counter()
+with urllib.request.urlopen(URL) as source:
+    for line in source:
+        words = re.split(r"[^A-Z]+", line.decode('utf-8'), flags=re.I)
+        counter.update(words)
+
+blankcounter = Counter()
+URL = 'https://en.wikipedia.org/wiki/Wikipedia:The_blank_page'
+with urllib.request.urlopen(URL) as source:
+    for line in source:
+        words = re.split(r"[^A-Z]+", line.decode('utf-8'), flags=re.I)
+        blankcounter.update(words)
+
+
+#remove keys from sea that are in emptyPage
+for key in blankcounter:
+    if key in counter:
+        del counter[key]
+
+print(counter)
+
+
+
+#Start a python GUI
+#Import the tkinter module
+from tkinter import *
+import random
+window = Tk()
+window.title("Lexical Race")
+window.geometry("900x700")
+#tkinter bigger font
+font1 = ("Times", 24, "bold")
+
+#Create a image
+
+
+horse2image = PhotoImage(file = "b-cavalo.png")
+horse2 = Label(window, image = horse2image)
+horse2.place(x = 36, y = 233.3333333333333)	
+window.update()
+
+horse1image = PhotoImage(file = "a-cavalo.png")
+horse1 = Label(window, image = horse1image)
+horse1.place(x = 36, y = 466.66666666667)	
+window.update()
+
+#create user input
+horse1x = 36
+userInput1 = Entry(window, width = 20, font=font1)
+userInput1.place(x = horse1x, y = 233 - 60)
+window.update()
+#create button to submit user input
+button1 = Button(window, text = "Submeter", font=font1, command= lambda : point(2))
+button1.place(x = 36, y = 233 - 120)
+window.update()
+
+horse2x = 36
+userInput2 = Entry(window, width = 20, font=font1)
+userInput2.place(x = horse2x, y = 466 + 70)
+window.update()
+
+def point(player):
+    if player == 1:
+        global horse1x
+        horse1x = horse1x + 40
+        horse1.place(x = horse1x, y = 466.3333333333333)
+        window.update()
+
+    if player == 2:
+        global horse2x
+        horse2x = horse2x + 40
+        horse2.place(x = horse2x, y = 233.66666666667)
+        window.update()
+
+window.mainloop()
